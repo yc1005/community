71f4dedbe48bf5ec7472e4d9976b5ef58514865b
yangce <942141056@qq.com>
2019-09-22 16:06:01 +0800

    init repo

---
+++ src/main/java/com/young/community/community/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.young.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name", defaultValue = "world") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
